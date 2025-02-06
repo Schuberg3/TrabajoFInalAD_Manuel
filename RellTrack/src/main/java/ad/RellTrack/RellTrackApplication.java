2b7d151f2e8d8933145c3e77dca38bff05dd1e7e
Schuberg3 <150267821+Schuberg3@users.noreply.github.com>
2025-02-06 11:05:03 +0100

    Inicio del proyecto

---
+++ RellTrack/src/main/java/ad/RellTrack/RellTrackApplication.java
@@ -0,0 +1,13 @@
+package ad.RellTrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RellTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RellTrackApplication.class, args);
+	}
+
+}
---
+++ RellTrack/src/test/java/ad/RellTrack/RellTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package ad.RellTrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RellTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
